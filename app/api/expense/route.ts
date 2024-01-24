3834598807ff339c3178d1f5bebd332333176246
Hemal Parmar <hemal.parmar@wizcart.com>
2024-01-24 14:21:45 +0530

    change db.ts

--- app/api/expense/route.ts
+++ app/api/expense/route.ts
@@ -5,8 +5,11 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   await connectDB();
   try {
-    const accounts = await Account.find({ isExpense: true });
-    return NextResponse.json({ data: accounts[0] }, { status: 200 });
+    const accounts = await Account.find();
+    return NextResponse.json(
+      { data: accounts.filter((account) => account.isExpense)[0] },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
