5db27945d33dd4de72eeead7a86ea2a5b6d28f7f
Hemal Parmar <hemal.parmar@wizcart.com>
2024-01-24 14:25:55 +0530

    change db.ts

--- app/api/expense/route.ts
+++ app/api/expense/route.ts
@@ -1,10 +1,10 @@
 import Account from "@/models/Account";
 import connectDB from "@/utils/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async ({ params }: { params: { id: string } }) => {
+export const GET = async (req: NextRequest) => {
   await connectDB();
-  console.log("params", params);
+  console.log("req", req);
   try {
     const accounts = await Account.find();
     return NextResponse.json(
