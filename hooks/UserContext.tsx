482ed781eee51d194e13978262d2dd7327f322f8
Hemal Parmar <hemal.parmar@wizcart.com>
2024-12-26 15:23:51 +0530

    build issue fix

--- hooks/UserContext.tsx
+++ hooks/UserContext.tsx
@@ -58,4 +58,10 @@ export const UserProvider = ({ children }: UserProviderProps) => {
   );
 };
 
-export const useUser = () => useContext(UserContext);
+export const useUser = (): UserContextType => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
